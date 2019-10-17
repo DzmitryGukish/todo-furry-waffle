9064c5eac0c7e8815658473a6410c24384daefac
DzmitryGukish <dzmitry.gukish@gmail.com>
2019-10-16 23:41:49 -0400

    Create index.js
    Write some simplest dummy test code

---
+++ index.js
@@ -0,0 +1,17 @@
+(function () {
+  init();
+  connect();
+  show();
+})();
+
+let init = function(){
+  console.log('init');
+};
+
+let connect = function(){
+  console.log('connect');
+};
+
+let show = function(){
+  console.log('show');
+};
